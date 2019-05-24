1904a8a3f2d806684e122cf85b664ff7960cb332
railgun1v9 <tempestblaze21@gmail.com>
2019-05-24 10:12:54 -0400

    Adding configuration file and service provider

--- src/LaraSPF/LaraSPFServiceProvider.php
+++ src/LaraSPF/LaraSPFServiceProvider.php
@@ -3,14 +3,31 @@
 namespace LaraSPF;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 class LaraSPFServiceProvider extends ServiceProvider
 {
+    use Operators\Filterable;
+
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/dingoquerymapper.php'   => config_path('dingoquerymapper.php'),
-        ], 'config');
+            __DIR__.'/../config/laraspf.php'   => config_path('laraspf.php'),
+        ]);
+        
+        // Macros
+        Builder::macro(config('filter.macros.filter'), function ($value = null) {
+            return Filterable::filter($value, $this);
+        });
+
+        Builder::macro(config('filter.macros.filterAndGet'), function ($value = null) {
+            return Filterable::filterAndGet($value, $this);
+        });
+
+        Collection::macro(config('filter.macros.filterAndGet'), function ($value = null) {
+            return Filterable::filterAndGetCollection($value, $this);
+        });
     }
 
     public function register()
@@ -23,6 +40,6 @@ class LaraSPFServiceProvider extends ServiceProvider
      */
     private function setupConfig()
     {
-        $this->mergeConfigFrom(realpath(__DIR__ . '/../../config/dingoquerymapper.php'), 'dingoquerymapper');
+        $this->mergeConfigFrom(realpath(__DIR__ . '/../config/laraspf.php'), 'laraspf');
     }
 }
---
+++ src/config/laraspf.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /* Macro Names*/
+    "macros" => [
+        "filter" => "filter",
+        "filterAndGet" => "filterAndGet"
+    ],
+
+    /* Keywords */
+    "keywords" => [
+        "sorting" => "sort",
+        "page_size" => "page_size",
+        "relationships" => "relationships",
+        "fields" => "fields",
+        "sum" => "sum",
+        "model_count" => "model_count"
+    ],
+
+    "relationship_separator" => "@",
+    "column_query_modificator" => "\/",
+    "paginate_by_default" => true,
+    "page_size" => 100,
+
+    "aditional_keywords" => [
+        "page"
+    ],
+
+    /*
+     * Are filter queries allowed? If set to true, queries like age>18 are allowed
+     */
+    'allowFilters' => false,
+
+    /*
+     * The default values
+     */
+    'defaults' => [
+        'limit' => 15,
+        'sort' => [
+            [
+                'column'    => 'id',
+                'direction' => 'asc'
+            ]
+        ],
+    ],
+
+    /*
+     * The parameters to be excluded
+     */
+    'excludedParameters' => [
+        'include',          // because of Fractal Transformers
+        'token',            // because of JWT Auth
+    ],
+];
